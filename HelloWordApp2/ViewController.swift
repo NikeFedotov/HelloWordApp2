1acbb383c37836673c90898cf4afb380937a3570
Nike Fedotov <nike.fedotoff@gmail.com>
2023-09-21 20:20:36 +0300

    Initial Commit

---
+++ HelloWordApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWordApp2
+//
+//  Created by Никита on 21.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
